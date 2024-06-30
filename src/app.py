e9125548a9b9a9460539426f2defbe3fa23255a9
xiang58 <danielxiang0508@gmail.com>
2024-06-29 21:07:03 -0500

    MSP-7 Create progress bar example

--- src/app.py
+++ src/app.py
@@ -1,7 +1,17 @@
 import streamlit as st
+import time
 
 def main():
-    st.text('Hello world')
+    big_to_go_box = '大便当盒'
+    big_to_go_box_bar = st.progress(100, big_to_go_box)
+
+    for percent_consumed in range(0, 101, 20):
+        big_to_go_box_bar.progress(100 - percent_consumed, text=big_to_go_box)
+        time.sleep(3)
+
+    time.sleep(3)
+    big_to_go_box_bar.empty()
+    st.button("Rerun")
 
 if __name__ == '__main__':
     main()
