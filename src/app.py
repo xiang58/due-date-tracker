e1dfbf7cac6cfa7e6c81c77760d02588657e0768
xiang58 <danielxiang0508@gmail.com>
2025-03-15 21:04:52 -0500

    MSP-21 Build POC

--- src/app.py
+++ src/app.py
@@ -1,23 +1,23 @@
+import pytz
 import streamlit as st
-import time
-
-
-def draw_progress_bar():
-    st.title("Streamlit Progress Bar Example")
-
-    # Create a progress bar object
-    progress_bar = st.progress(0)
-
-    # Simulate a task progressing
-    for percent_complete in range(101):
-        time.sleep(0.05)  # Simulating work
-        progress_bar.progress(percent_complete)  # Update progress
-
-    st.success("Task Completed! 🎉")
+from datetime import date, datetime
 
 
 def main():
-    draw_progress_bar()
+    draw_progress_bar('bath towel', '2025-03-14', 4)
+
+
+def draw_progress_bar(desc, reset_date_str, period):
+    date_delta = compute_date_delta(reset_date_str)
+    progress_val = date_delta / period
+    st.progress(progress_val, f'{desc} ({progress_val * 100}%)')
+
+
+def compute_date_delta(reset_date_str):
+    cst = pytz.timezone('America/Chicago')
+    today = datetime.now(cst).date()
+    reset_date = date.fromisoformat(reset_date_str)
+    return (today - reset_date).days
 
 
 if __name__ == '__main__':
