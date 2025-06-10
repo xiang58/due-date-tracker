b99376c94bcf7ea3bee12f69bfb3aa9c3f9ff7df
xiang58 <danielxiang0508@gmail.com>
2025-06-10 16:39:41 -0500

    MSP-22 Add reset button

--- src/app.py
+++ src/app.py
@@ -7,15 +7,41 @@ import helper
 
 
 def main():
+    if 'show_dialog' not in st.session_state:
+        st.session_state['show_dialog'] = None
+
     recs = helper.get_all_recs()
     for rec in recs:
-        draw_progress_bar(rec['desc'], rec['reset_dt'], rec['period'])
+        draw_progress_bar(rec)
+
+        reset_key = f"reset_{rec['id']}"
+        if st.button("Reset", key=reset_key, disabled=is_reset_button_disabled(rec)):
+            st.session_state['show_dialog'] = rec['id']
+            st.rerun()
+
+        if st.session_state.get('show_dialog') == rec['id']:
+            render_dt_picker(rec)
+
+        st.write('')
+        st.write('')
 
 
-def draw_progress_bar(desc, reset_date_str, period):
-    date_delta = compute_date_delta(reset_date_str)
-    progress_val = float(date_delta / period)
-    st.progress(progress_val, f'{desc} ({round(progress_val * 100)}%)')
+def draw_progress_bar(rec):
+    date_delta = compute_date_delta(rec['reset_dt'])
+    progress_val = min(float(date_delta / rec['period']), 1.0)
+    st.progress(progress_val, f'{rec['desc']} ({round(progress_val * 100)}%) - last reset date: {rec['reset_dt']}')
+
+
+def is_reset_button_disabled(rec):
+    return st.session_state['show_dialog'] is not None and st.session_state['show_dialog'] != rec['id']
+
+
+def render_dt_picker(rec):
+    new_reset_dt = st.date_input('Pick a reset date:', key=f"date_{rec['id']}", max_value=date.today()).isoformat()
+    if st.button("Confirm", key=f"confirm_{rec['id']}"):
+        helper.update_reset_dt(rec, new_reset_dt)
+        st.session_state['show_dialog'] = None
+        st.rerun()
 
 
 def compute_date_delta(reset_date_str):
--- src/helper.py
+++ src/helper.py
@@ -5,8 +5,8 @@ import streamlit as st
 def get_dynamodb_table():
     dynamodb = boto3.resource(
         'dynamodb', region_name='us-east-1', 
-        aws_access_key_id=st.secrets["aws_access_key_id"],
-        aws_secret_access_key=st.secrets["aws_secret_access_key"]
+        aws_access_key_id=st.secrets['aws_access_key_id'],
+        aws_secret_access_key=st.secrets['aws_secret_access_key']
     )
     return dynamodb.Table('due-date-tracker')
 
@@ -17,3 +17,17 @@ def get_all_recs():
     items = response['Items']
     items.sort(key=lambda x: x['period'])
     return items
+
+
+def update_reset_dt(rec, new_reset_dt):
+    table = get_dynamodb_table()
+    response = table.update_item(
+        Key={'id': rec['id']},
+        UpdateExpression='SET reset_dt = :val',
+        ExpressionAttributeValues={':val': new_reset_dt}
+    )
+
+    if response['ResponseMetadata']['HTTPStatusCode'] == 200:
+        st.toast('Item inserted successfully!', icon='🎉')
+    else:
+        st.toast('Error inserting item:' + str(response), icon='🚨')
