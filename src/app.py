64ce2e788dcc36ae7e9fb73c38866bbd4c4d3fa9
xiang58 <danielxiang0508@gmail.com>
2025-06-10 16:57:58 -0500

    MSP-22 Add dialog for reset button

--- src/app.py
+++ src/app.py
@@ -7,21 +7,10 @@ import helper
 
 
 def main():
-    if 'show_dialog' not in st.session_state:
-        st.session_state['show_dialog'] = None
-
     recs = helper.get_all_recs()
     for rec in recs:
         draw_progress_bar(rec)
-
-        reset_key = f"reset_{rec['id']}"
-        if st.button("Reset", key=reset_key, disabled=is_reset_button_disabled(rec)):
-            st.session_state['show_dialog'] = rec['id']
-            st.rerun()
-
-        if st.session_state.get('show_dialog') == rec['id']:
-            render_dt_picker(rec)
-
+        st.button('Reset', key=rec['id'], on_click=render_dt_picker, args=(rec,))
         st.write('')
         st.write('')
 
@@ -32,15 +21,11 @@ def draw_progress_bar(rec):
     st.progress(progress_val, f'{rec['desc']} ({round(progress_val * 100)}%) - last reset date: {rec['reset_dt']}')
 
 
-def is_reset_button_disabled(rec):
-    return st.session_state['show_dialog'] is not None and st.session_state['show_dialog'] != rec['id']
-
-
+@st.dialog('Reset Date')
 def render_dt_picker(rec):
-    new_reset_dt = st.date_input('Pick a reset date:', key=f"date_{rec['id']}", max_value=date.today()).isoformat()
-    if st.button("Confirm", key=f"confirm_{rec['id']}"):
+    new_reset_dt = st.date_input(f'Pick a reset date for {rec['desc']}:', max_value=date.today()).isoformat()
+    if st.button('Confirm'):
         helper.update_reset_dt(rec, new_reset_dt)
-        st.session_state['show_dialog'] = None
         st.rerun()
 
 
